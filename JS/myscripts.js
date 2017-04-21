ceace997beef20d3ee25237a0b4746d9a0a3c109
Javier Armero <Javalba90@gmail.com>
2017-04-21 22:54:33 +0200

    ironhack --> learning_unit/689

---
+++ JS/myscripts.js
@@ -0,0 +1,61 @@
+var name = "Josh";
+var age;
+age = 45;
+console.log(name);
+console.log(age);
+
+
+var myAge = 25;
+
+/*BASIC OPERATORS*/
+console.log("BASIC OPERATORS");
+console.log(2 + 2);
+console.log(4 - 2);
+console.log(3 * 2);
+console.log(6 / 2);
+
+
+// 4 / 2 = 2
+console.log(4 / 2);
+//With a remainder of 0
+console.log(4 % 2);
+
+// 7 / 2 = 3.5
+console.log(7 / 2);
+//With a remainder of 1
+console.log(7 % 2);
+
+// If a number modulus other number is equal to 0
+// it is a multiple of "other number"
+
+// 8 is indeed a multiple of 2!
+console.log(8 % 2 === 0);
+// 9 is NOT a multiple of 2!
+console.log(9 % 2 === 0);
+
+myAge += 1;
+console.log(myAge);
+
+
+var i2 = 10 + 5 * 2 ** 3 / 4 - 6;
+console.log(i2);
+
+//  === 10 + 5 * 8 / 4 - 6
+//  === 10 + 5 * 2 - 6
+//  === 10 + 10 - 6
+//  === 10 + 4
+//  ==> 14
+
+
+
+var expressionOne = ((2 * 2) + 5) * 6;
+ console.log("54-->((2 * 2) + 5) * 6 --> "+expressionOne);
+
+var expressionTwo = ((2* 2) + (5 * 3)) - 5;
+ console.log("14-->((2* 2) + (5 * 3)) - 5 --> "+expressionTwo);
+
+var expressionThree = (5 * 5) / (5 * 5);
+ console.log("1 -->(5 * 5) / (5 * 5) --> "+expressionThree);
+
+var expressionFour = 5 * 5 - 5 * 4;
+ console.log("5-->5 * 5 - 5 * 4 --> "+expressionFour);
